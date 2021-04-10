8c37d68185805d40b565fb1da66675ba16a0c69f
wendong <5917384949@qq.com>
2021-04-10 15:17:04 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(porps) {
+    super(porps)
+    this.socketInit()
+  }
+  
+  state= {
+    
+  }
+
+  socketInit() {
+    console.log('socketInit');
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        123123
+      </div>
+    )
+  }
 }
+
 
 export default App;
